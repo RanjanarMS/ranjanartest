d795aaa9d9d85cc0e1124eff4cae08dea60f299d
ranjana <ranjanar@microsoft.com>
2017-01-19 14:47:37 +0530

    My test

---
+++ Aut/Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Aut;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+            [TestMethod]
+        public void TestAdd()
+        {
+            int number1 = 10;
+            int number2 = 5;
+            Calc calc = new Calc();
+            int sum = calc.Add(number1, number2);
+            Assert.AreEqual(sum, 15);
+        }
+    }
+}
