eff41411c412ccf4d180b46c8f9a26d52e146528
ranjana <ranjanar@microsoft.com>
2017-02-14 16:29:33 +0530

    All the configs

--- Aut/Test2/UnitTest1.cs
+++ Aut/Test2/UnitTest1.cs
@@ -1,16 +1,24 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using Aut;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Test
+namespace Test2
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest2
     {
+        [ClassInitialize]
+        public static void ClassSetup(TestContext context)
+        {
+            webUrl = (string)context.Properties["webAppUrl"];
+            webAppUserName = (string)context.Properties["-webAppUserName"];
+        }
+
         [TestMethod]
         [TestCategory("Odd"), TestCategory("EvenOdd")]
-        public void TestAdd11()
+        public void TestAdd21()
         {
             double number1 = 10.1;
             double number2 = 5.2;
@@ -21,7 +29,7 @@ namespace Test
 
         [TestMethod]
         [TestCategory("Even"), TestCategory("EvenOdd")]
-        public void TestAdd12()
+        public void TestAdd22()
         {
             double number1 = 10.1;
             double number2 = 5.2;
@@ -33,7 +41,7 @@ namespace Test
 
         [TestMethod]
         [TestCategory("Odd"), TestCategory("EvenOdd")]
-        public void TestAdd13()
+        public void TestAdd23()
         {
             double number1 = 10.1;
             double number2 = 5.2;
@@ -45,7 +53,7 @@ namespace Test
 
         [TestMethod]
         [TestCategory("Even"), TestCategory("EvenOdd")]
-        public void TestAdd14()
+        public void TestAdd24()
         {
             int number1 = 10;
             int number2 = 5;
@@ -57,7 +65,7 @@ namespace Test
 
         [TestMethod]
         [TestCategory("Odd"), TestCategory("EvenOdd")]
-        public void TestAdd15()
+        public void TestAdd25()
         {
             int number1 = 10;
             int number2 = 5;
@@ -68,18 +76,45 @@ namespace Test
 
         [TestMethod]
         [TestCategory("Even"), TestCategory("EvenOdd")]
-        public void TestAdd16()
+        public void TestAdd26()
         {
             int number1 = 10;
             int number2 = 5;
             Calc calc = new Calc();
             int sum = calc.Add(number1, number2);
+            Assert.AreEqual(sum, 15);
+        }
+
+        [TestMethod]
+        [TestCategory("RunSettingsTest")]
+        public void TestMethodRunSettings()
+        {
+            int number1 = 10;
+            int number2 = 5;
+            Calc calc = new Calc();
+            int sum = calc.Add(number1, number2);
+            Assert.AreEqual(webUrl, "http://localhost");
+            Assert.AreEqual(webAppUserName, "Admin");
+            Assert.AreEqual(sum, 15);
+        }
+
+
+        [TestMethod]
+        [TestCategory("RunSettingsTestOverride")]
+        public void TestMethodRunSettingsOverride()
+        {
+            int number1 = 10;
+            int number2 = 5;
+            Calc calc = new Calc();
+            int sum = calc.Add(number1, number2);
+            Assert.AreEqual(webUrl, "http://override");
+            Assert.AreEqual(webAppUserName, "--override-Admin--;blah");
             Assert.AreEqual(sum, 15);
         }
 
         [TestMethod]
         [TestCategory("RunInParallel")]
-        public void TestMethodRunInParallel1()
+        public void TestMethodRunInParallel2()
         {
             int number1 = 10;
             int number2 = 5;
@@ -88,6 +123,23 @@ namespace Test
             Thread.Sleep(5 * 60 * 1000);
             Assert.AreEqual(sum, 15);
         }
+
+        [TestMethod]
+        [TestCategory("RunInIsolation")]
+        public void TestMethodRunInIsolation()
+        {
+            int number1 = 10;
+            int number2 = 5;
+            Calc calc = new Calc();
+            int sum = calc.Add(number1, number2);
+            Process currentProcess = Process.GetCurrentProcess();
+            Console.WriteLine("The process Id is :" + currentProcess.Id);
+            Console.WriteLine("The process name is :" + currentProcess.ProcessName);
+            Console.WriteLine("The process running in the box :" + currentProcess.MachineName);
+            Assert.AreEqual(sum, 15);
+        }
+        private static string webUrl = string.Empty;
+        private static string webAppUserName = string.Empty;
 
     }
 }
