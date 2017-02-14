06b6723a04bf8e52b39032bfa3e1a668de8998d1
ranjana <ranjanar@microsoft.com>
2017-02-14 15:01:17 +0530

    Adding UI tests as well

---
+++ Aut/CalcApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text3 = "Wrong Input";
+            if( !string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                int number1;
+                int number2;
+                if(int.TryParse(textBox1.Text, out number1) && int.TryParse(textBox2.Text, out number2))
+                {
+                    text3 = (number1 + number2).ToString();
+                }
+            }
+            textBox3.Text = text3;
+        }
+    }
+}
